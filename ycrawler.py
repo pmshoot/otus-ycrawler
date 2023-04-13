1ac94d44e07700205c9f74a7690d58c9777eb79e
Michael <petroff.mb@gmail.com>
2023-04-13 18:16:35 +0500

    Update ycrawler.py

--- ycrawler.py
+++ ycrawler.py
@@ -45,18 +45,17 @@ async def read_url(url, session, semafor):
     return response, content_type
 
 
-async def write_file(fp: Path, content):
+async def write_file(fp: Path, content, mode='wb'):
     """"""
     if not await aiospath.exists(fp.parent):
         await aios.makedirs(fp.parent, exist_ok=True)
-    async with aiofiles.open(fp, mode='wb') as f:
+    async with aiofiles.open(fp, mode=mode) as f:
         await f.write(content)
 
 
 async def handle_record(id, record, session, semafor):
     url = f'{ycomb_url}item?id={id}'
     response, content_type = await read_url(url, session, semafor)
-    # logging.info(f'Item: {url}')
     if not response:
         return
     # write record to file
@@ -70,7 +69,12 @@ async def handle_record(id, record, session, semafor):
 
     bs = BeautifulSoup(response, features="html.parser")
     st = bs.table.find(attrs={'class': 'comment-tree'})
-    items = st.find_all('tr', attrs={'class': 'athing'})
+    items = st.find_all('tr', attrs={'class': 'athing'})  #
+    if not items:
+        return
+    comm_index = collections.defaultdict(list)
+    comm_index_dir = Path(store_path) / id / 'comments'
+    comm_index_file = comm_index_dir / 'index.json'
 
     for tr in items:
         comm_id = tr['id']
@@ -80,13 +84,10 @@ async def handle_record(id, record, session, semafor):
         comm_urls = [a['href'] for a in commtext.find_all('a') if a]
         if not comm_urls:  # no links in comment
             continue
-        comm_index = collections.defaultdict(list)
-        comm_index_dir = Path(store_path) / id / 'comments'
-        comm_index_file = comm_index_dir / 'index.json'
         for comm_url in comm_urls:
             # download html by comment link
             response, content_type = await read_url(comm_url, session, semafor)
-            logging.info(f'Comm: {comm_url}')
+            logging.info(f'Comm: {comm_id}\t{comm_url}')
             if not response:
                 logging.debug(f'{id}::{comm_id}: error get data from comment url {comm_url}')
                 continue
@@ -98,6 +99,7 @@ async def handle_record(id, record, session, semafor):
             fp = comm_index_dir / fname
             await write_file(fp, response)
             comm_index[comm_id].append([fname, comm_url])
+    if comm_index:
         await write_file(comm_index_file, json.dumps(comm_index).encode(encoding=encoding))
     return name, url
 
@@ -111,8 +113,6 @@ async def main(options):
             parsed = json.loads(fp.read())
     except Exception:
         parsed = {}
-
-    # try:
     while True:
         async with aiohttp.ClientSession() as session:
             try:
@@ -122,22 +122,28 @@ async def main(options):
                     break
                 bs = BeautifulSoup(response, features="html.parser")
                 records = bs.table.find_all(attrs={'class': 'athing'})
-                for record in records:
-                    id = record['id']
-                    if id in parsed:  # no repeat
+                if not records:
+                    logging.info('Root: не найдены новости')
+                    break
+                for record in records[:options.numbers]:
+                    try:
+                        id = record['id']
+                        if id in parsed:  # no repeat
+                            continue
+                        name_url = await handle_record(id, record, session, semafor)
+                        if not name_url:
+                            continue
+                        parsed[id] = name_url
+                    except KeyboardInterrupt:
+                        break
+                    except Exception as e:
+                        logging.error(f'main: {e}')
                         continue
-                    name, url = await handle_record(id, record, session, semafor)
-                    if not name:
-                        continue
-                    parsed[id] = name, url
-            except KeyboardInterrupt:
-                break
-            except Exception as e:
-                logging.error(f'main: {e}')
-                continue
             finally:
                 with parsed_fp.open('wt', encoding=encoding) as fp:
                     json.dump(parsed, fp)
+            logging.info('--- End cycle ------------------------')
+
         logging.info(f'Waiting for {options.period} sec...')
         await asyncio.sleep(options.period)
         logging.info('Repeat')
@@ -145,12 +151,15 @@ async def main(options):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--period', type=int, default=30, action='store')
-    parser.add_argument('-t', '--max_tasks', type=int, default=5, action='store')
+    parser.add_argument('-p', '--period', type=int, default=30, action='store', help='Период повтора опроса в секундах')
+    parser.add_argument('-t', '--max_tasks', type=int, default=5, action='store', help='Максимальное количество '
+                                                                                       'одновременных загрузок')
+    parser.add_argument('-n', '--numbers', type=int, default=30, action='store', help='Количество обрабатываемых '
+                                                                                    'новостей на главной странице')
     parser.add_argument('-d', '--debug', action='store_true')
     options = parser.parse_args()
     logging.basicConfig(level=logging.DEBUG if options.debug else logging.INFO)
     #
-    while True:
-        asyncio.run(main(options))
+    asyncio.run(main(options))
+
 
